cf44b235624136c69ec8a12b6074f0872c64a659
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-03 19:39:36 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,15 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorised() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - abort with 401 customized error page
+    """
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
