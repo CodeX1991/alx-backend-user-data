06dd00643e190f4dbfee65b77fca7fce2822f847
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-05 23:23:34 +0000

    Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -61,3 +62,12 @@ class Auth:
             None
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie vale from a request"""
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+
+        return request.cookies.get(session_name)
