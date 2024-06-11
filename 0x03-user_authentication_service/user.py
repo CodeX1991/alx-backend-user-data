dd03fd44ca15e1f7c8ba1e022d6011dc6678a516
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-11 19:19:37 +0000

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""User authentication"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Create an instance of the declaration
+Base = declarative_base()
+
+
+class User(Base):
+    """A user model for the table=users"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
