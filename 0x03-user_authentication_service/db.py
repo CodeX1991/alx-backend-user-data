a4767ad85bd9bc089480c2a9a550e6e9aa2a26e0
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-12 19:04:12 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import Base, User
 
@@ -47,3 +48,24 @@ class DB:
         session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        A search method
+
+        Args:
+            kwargs: varadic arguments
+        Returns:
+            The first row found in the users table,
+            otherwise raise corresponding exceptions
+        """
+        session = self._session
+
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+
+        return user
